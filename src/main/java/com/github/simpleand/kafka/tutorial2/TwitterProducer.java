c4d25d92043bcd23d11280e0f4a250f2809198cc
Andre Ramos da Silva <and.rds@gmail.com>
2019-01-07 17:56:12 -0200

    indempotent producer properties

--- src/main/java/com/github/simpleand/kafka/tutorial2/TwitterProducer.java
+++ src/main/java/com/github/simpleand/kafka/tutorial2/TwitterProducer.java
@@ -10,9 +10,16 @@ import com.twitter.hbc.core.processor.StringDelimitedProcessor;
 import com.twitter.hbc.httpclient.auth.Authentication;
 import com.twitter.hbc.httpclient.auth.OAuth1;
 import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.TimeUnit;
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,6 +35,8 @@ public class TwitterProducer {
     private String token = "284488001-kRwoShps9C9mEYQWb7Uk6IOFi7nAIoA2iqxp8Io5";
     private String secret = "sLBJ3HM9EB0E5KyZ1Gg96EVooso5ukA5alCeCk17GaDAA";
 
+    List<String> terms = Lists.newArrayList("bitcoin","usa","sport","soccer","politics");
+
     public static void main(String[] args) {
         new TwitterProducer().run();
     }
@@ -39,6 +48,17 @@ public class TwitterProducer {
         BlockingDeque<String> msgQueue = new LinkedBlockingDeque<>(1000);
         Client client = createTwitterClient(msgQueue);
         client.connect();
+
+        KafkaProducer<String, String> producer = createKafkaProducer();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(()->{
+            logger.info("Stoping application...");
+            logger.info("Shutting down twitter client...");
+            client.stop();
+            logger.info("Closing producer...");
+            producer.close();
+            logger.info("Done!");
+        }));
 
         while (!client.isDone()) {
 
@@ -52,7 +72,17 @@ public class TwitterProducer {
             }
 
             if (msg != null) {
+
                 logger.info(msg);
+
+                producer.send(new ProducerRecord<>("twitter_tweets", null, msg), new Callback() {
+                    @Override
+                    public void onCompletion(final RecordMetadata recordMetadata, final Exception e) {
+                        if(e != null) {
+                            logger.error("Something bag happened", e);
+                        }
+                    }
+                });
             }
 
         }
@@ -64,7 +94,6 @@ public class TwitterProducer {
         Hosts hoseBirdHosts = new HttpHosts(Constants.STREAM_HOST);
         StatusesFilterEndpoint hoseBirdEndpoint = new StatusesFilterEndpoint();
 
-        List<String> terms = Lists.newArrayList("bitcoin");
         hoseBirdEndpoint.trackTerms(terms);
 
         Authentication hoseBirdAuth = new OAuth1(consumerKey, consumerSecret, token, secret);
@@ -78,5 +107,30 @@ public class TwitterProducer {
 
         Client hoseBirdClient = builder.build();
         return hoseBirdClient;
+    }
+
+    public KafkaProducer<String, String> createKafkaProducer() {
+        final String bootstrapServers = "127.0.0.1:9092";
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+
+        //Configs to indempotent producer - safe producer
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+        //kafka 2.2 >= 1.1 so we can keep this as 5. Use 1 otherwise.
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+
+        //data compression by bach
+        //high throughput producer (at the expense of a bit of latency and CPU message)
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20"); //20ms of linger or delay
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32*1024)); //32 KB batch size
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+        return producer;
     }
 }
